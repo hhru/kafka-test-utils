41f1f2a04a40fa51f7bbc554b2988986644bf358
boksh <e.bokshitskiy@hh.ru>
2020-04-27 20:57:00 +0300

    HH-108442 allow to overwrite kafka-broker settings

--- src/main/java/ru/hh/kafka/test/KafkaTestUtils.java
+++ src/main/java/ru/hh/kafka/test/KafkaTestUtils.java
@@ -2,20 +2,28 @@ package ru.hh.kafka.test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Duration;
+import java.util.Collections;
 import static java.util.Collections.emptyMap;
 import java.util.Map;
 import org.testcontainers.containers.KafkaContainer;
 
 public class KafkaTestUtils {
 
-  public static KafkaContainer startKafkaContainer() {
+  public static KafkaContainer startKafkaContainer(Map<String, String> additionalConfigs) {
     KafkaContainer kafkaContainer = new KafkaContainer().withEmbeddedZookeeper();
+    // Overwrite broker settings using env variables
+    // https://docs.confluent.io/current/installation/docker/config-reference.html#confluent-kafka-configuration
+    additionalConfigs.forEach((key, value) -> kafkaContainer.withEnv("KAFKA_" + key.replace(".", "_").toUpperCase(), value));
     kafkaContainer.start();
     return kafkaContainer;
   }
 
   public static TestKafka startKafka() {
-    return connectToKafka(startKafkaContainer().getBootstrapServers(), emptyMap(), emptyMap());
+    return connectToKafka(startKafkaContainer(Collections.emptyMap()).getBootstrapServers(), emptyMap(), emptyMap());
+  }
+
+  public static TestKafka startKafka(Map<String, String> additionalConfigs) {
+    return connectToKafka(startKafkaContainer(additionalConfigs).getBootstrapServers(), emptyMap(), emptyMap());
   }
 
   public static TestKafka connectToKafka(String bootstrapServers,
@@ -32,7 +40,11 @@ public class KafkaTestUtils {
   }
 
   public static TestKafkaWithJsonMessages startKafkaWithJsonMessages(ObjectMapper objectMapper) {
-    return connectToKafkaWithJsonMessages(startKafkaContainer().getBootstrapServers(), emptyMap(), emptyMap(), objectMapper);
+    return connectToKafkaWithJsonMessages(startKafkaContainer(Collections.emptyMap()).getBootstrapServers(), emptyMap(), emptyMap(), objectMapper);
+  }
+
+  public static TestKafkaWithJsonMessages startKafkaWithJsonMessages(ObjectMapper objectMapper, Map<String, String> additionalConfigs) {
+    return connectToKafkaWithJsonMessages(startKafkaContainer(additionalConfigs).getBootstrapServers(), emptyMap(), emptyMap(), objectMapper);
   }
 
   public static TestKafkaWithJsonMessages connectToKafkaWithJsonMessages(String bootstrapServers,
--- src/test/java/ru/hh/kafka/test/TestBase.java
+++ src/test/java/ru/hh/kafka/test/TestBase.java
@@ -1,9 +1,10 @@
 package ru.hh.kafka.test;
 
+import java.util.Collections;
 import org.testcontainers.containers.KafkaContainer;
 
 public class TestBase {
 
-  protected static KafkaContainer kafkaContainer = KafkaTestUtils.startKafkaContainer();
+  protected static KafkaContainer kafkaContainer = KafkaTestUtils.startKafkaContainer(Collections.emptyMap());
 
 }
